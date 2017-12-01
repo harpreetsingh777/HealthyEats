e5479393291766d50d70972aebacc43d13a2bc42
Christine Kao <xtinekao@gmail.com>
2017-12-01 16:34:56 -0600

    Added search by ingredient incomplete

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import {Input, Button} from 'semantic-ui-react'
 
+import styles from './Home.css'
 class Home extends Component {
 	constructor() {
         super();
         this.state = {
-            recipes: []
+            value: "",
+            recipes: [],
+            filteredRecipes: [],
+            filteredIngredients: []
         };
+        this.inputChangeHandler = this.inputChangeHandler.bind(this);
+        this.clickHandler = this.clickHandler.bind(this);
     }
 
     componentDidMount() {
@@ -22,12 +29,44 @@ class Home extends Component {
 	        })
   	}
 
+    inputChangeHandler(event){
+        this.setState({value: event.target.value});
+    }
+
+    clickHandler(){
+        let tempRecipe = [];
+        let tempIngredients = [];
+        this.state.recipes.map((recipe) =>{
+            let recipeURL = '/recipes/ingredients/' + recipe.recipe_name + "/"
+            console.log(recipeURL)
+            fetch(recipeURL)
+            .then((response) => response.json())
+            .then((jsonResponse) => jsonResponse.data)
+            .then((ingredientsData) =>{
+                ingredientsData.map( (ingredient) => {
+                    if(ingredient.name == this.state.value){
+                        tempRecipe.push(recipe);
+                        tempIngredients.push(ingredientsData);
+                        this.setState({
+                            filteredRecipes: tempRecipe,
+                            filteredIngredients: tempIngredients
+                        })
+                    }
+                })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        })
+    }
     render() {
         return(
             <div className="Home">
                 <h1>Welcome to HealthyEats!</h1>
+                 <Input onChange = {this.inputChangeHandler} placeholder = "Put in an ingredient!" value = {this.state.value} />
+                <Button onClick = {this.clickHandler}>Search</Button>
                 {this.state.recipes.map(recipe =>
-          			<div key={recipe.name}>{recipe.name}</div>
+          			<div key={recipe.name}>Hi</div>
         		)}
             </div>
         )
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {HashRouter as Router, Route, Link, Switch} from 'react-router-dom'
 import Home from './components/Home.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<Home />, 
+				document.getElementById('root'));
 registerServiceWorker();
