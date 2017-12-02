ece6f5915e8e429cf188cf3c1baa2a9fff51b023
Christine Kao <xtinekao@gmail.com>
2017-12-02 02:34:37 -0600

    Added search by recipe name, Card not rendering for some reason and need to implement filter by calories and diet restriction

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
-import {Input, Button} from 'semantic-ui-react'
-
+import {Input, Button, Card} from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 import styles from './Home.css'
+import RecipeView from './RecipeView.jsx'
+
 class Home extends Component {
 	constructor() {
         super();
         this.state = {
             value: "",
             recipes: [],
-            filteredRecipes: [],
-            filteredIngredients: []
+            filteredRecipe: [],
+            calories: "",
+            restriction: ""
         };
         this.inputChangeHandler = this.inputChangeHandler.bind(this);
         this.clickHandler = this.clickHandler.bind(this);
+        this.clickAdvanceHandler = this.clickAdvanceHandler.bind(this)
+        this.inputCalorieChangeHandler = this.inputCalorieChangeHandler.bind(this)
+        this.inputDietChangeHandler = this.inputDietChangeHandler.bind(this)
     }
 
     componentDidMount() {
@@ -34,40 +40,83 @@ class Home extends Component {
     }
 
     clickHandler(){
-        let tempRecipe = [];
         let tempIngredients = [];
+        let tempRecipes = [];
         this.state.recipes.map((recipe) =>{
-            let recipeURL = '/recipes/ingredients/' + recipe.recipe_name + "/"
-            console.log(recipeURL)
-            fetch(recipeURL)
-            .then((response) => response.json())
-            .then((jsonResponse) => jsonResponse.data)
-            .then((ingredientsData) =>{
-                ingredientsData.map( (ingredient) => {
-                    if(ingredient.name == this.state.value){
-                        tempRecipe.push(recipe);
-                        tempIngredients.push(ingredientsData);
-                        this.setState({
-                            filteredRecipes: tempRecipe,
-                            filteredIngredients: tempIngredients
-                        })
-                    }
+            if (recipe.recipe_name === this.state.value){
+                let tempRecipe = {
+                    recipe_name: recipe.recipe_name,
+                    image_url: recipe.image_url,
+                    calories: recipe.calories,
+                    ingredients: []
+                }
+                let recipeURL = '/recipes/ingredients/' + recipe.recipe_name + "/"
+                fetch(recipeURL)
+                .then((response) => response.json())
+                .then((jsonResponse) => jsonResponse.data)
+                .then((ingredientsData) =>{
+                    ingredientsData.map( (ingredient) => {
+                        tempIngredients.push(ingredient.ingredient_name);
+                        tempRecipe.ingredients = tempIngredients
+                    })
+                    tempRecipes.push(tempRecipe);
+                    this.setState({ filteredRecipe: tempRecipes})
+                    console.log(this.state.filteredRecipe)
                 })
-            })
-            .catch((error) => {
-                console.log(error);
-            })
+                .catch((error) => {
+                    console.log(error);
+                })
+            }
         })
     }
+
+    inputCalorieChangeHandler(event){
+        this.setState({calorie: event.target.value})
+    }
+
+    inputDietChangeHandler(event){
+        this.setState({ restriction: event.target.value})
+    }
+//Need to finish filtering by calories and dietary restrictions
+
+    clickAdvanceHandler(){
+        let tempIngredients = [];
+        this.state.recipes.map((recipe) =>{
+            if (this.state.calories.length == 0 || recipe.calories <= this.state.calories){
+                let recipeURL = '/recipes/ingredients/' + recipe.recipe_name + "/"
+                fetch(recipeURL)
+                .then((response) => response.json())
+                .then((jsonResponse) => jsonResponse.data)
+                .then((ingredientsData) =>{
+                    ingredientsData.map( (ingredient) => {
+                        tempIngredients.push(ingredient.ingredient_name);
+                        this.setState({
+                            filteredIngredients: tempIngredients
+                        })
+                        console.log(tempIngredients);
+                    })
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+            }
+        })
+    }
+    //RecipeView not rendering for some reason
     render() {
         return(
             <div className="Home">
                 <h1>Welcome to HealthyEats!</h1>
-                 <Input onChange = {this.inputChangeHandler} placeholder = "Put in an ingredient!" value = {this.state.value} />
+                 <Input onChange = {this.inputChangeHandler} placeholder = "Put in a recipe!" value = {this.state.value} />
                 <Button onClick = {this.clickHandler}>Search</Button>
-                {this.state.recipes.map(recipe =>
-          			<div key={recipe.recipe_name}>{recipe.recipe_name}</div>
-        		)}
+                <h3> OR </h3>
+                 <Input onChange = {this.inputCalorieChangeHandler} placeholder = "Put in a calorie amount!" value = {this.state.calories} />
+                <Input onChange = {this.inputDietChangeHandler} placeholder = "Put in a dietary restriction!" value = {this.state.restriction} />
+                 <Button onClick = {this.clickAdvanceHandler}>Search</Button>
+                 { this.state.filteredRecipe.map((recipe) => {
+                    <RecipeView recipe = {recipe}></RecipeView>
+                    })
+                } 
             </div>
         )
     }
